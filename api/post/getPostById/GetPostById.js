a6fad82bd8eb04300b7c6073430f7b3e3a2ea71b
Jae Woong Lee <dreamsctv@naver.com>
2020-05-25 11:24:01 +0900

    getPostById add

---
+++ api/post/getPostById/GetPostById.js
@@ -0,0 +1,26 @@
+import Post from '../../../database/model/Post';
+
+export default {
+    Query: {
+        getPostById: async (_ , {postId}) => {
+            try {
+                const post = await Post.findById({_id: postId}).populate({
+                    path: "user",
+                    model: "User",
+                    select: "id name email"
+                });
+                return {
+                    success: true,
+                    error: null,
+                    data: post
+                }
+            } catch (error) {
+                return {
+                    success: false,
+                    error: error.message,
+                    data: null
+                }
+            }
+        }
+    }
+}
