08653cf112eb2a01407782ca63a10df27340e5ab
Jae Woong Lee <dreamsctv@naver.com>
2020-05-25 15:56:35 +0900

    update

--- api/post/deletePostById/DeletePostById.js
+++ api/post/deletePostById/DeletePostById.js
@@ -6,23 +6,34 @@ export default {
         deletePostById: async (_, {postId}, { request, isAuthenticated }) => {
             isAuthenticated(request);
             const { userId: currentUserId  } = request;
+            try{
+                let post = await Post.findById({ _id: request.params.postId });
+                // post가 존재하지 않을 때
+                if (!post) {
+                    return {
+                        success: false,
+                        error: '포스트가 존재하지 않습니다.',
+                    }}
 
-            let post = await Post.findById({ _id: request.params.postId });
-            // post가 존재하지 않을 때
-            if (!post) {
+                if (post.user.toString() !== currentUserId ) {
+                    return {
+                        success: false,
+                        error: '해당 권한이 없습니다.',
+                        data: post
+                    }}
+                    
+                post = await Post.findByIdAndDelete({ _id: postId });
+
+                return {
+                    success: true,
+                    error: null
+                };
+            } catch (error) {
                 return {
                     success: false,
-                    error: '포스트가 존재하지 않습니다.',
-                  }}
-
-            if (post.user.toString() !== currentUserId ) {
-                return {
-                    success: false,
-                    error: '해당 권한이 없습니다.',
-                    data: post
-                  }}
-                
-            post = await Post.findByIdAndDelete({ _id: postId });
+                    error: error.message
+                };
+            }
         }
     }
 }
