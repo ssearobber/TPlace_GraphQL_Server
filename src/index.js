1adf1feb38af0619ead67ec506f153896ff90b39
ssearobber <ssearobber@naver.com>
2020-06-28 21:42:45 +0900

    이미지 업로드

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './env';
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
+import cors from 'cors';
 import schema from './schema';
 import connectDB from './database/mongoDB/util/connectDB';
 import { isAuthenticated } from './middlewares/auth';
+import { uploadMiddleware, uploadController } from './util/upload';
 
 //DB연결
 connectDB();
@@ -15,5 +17,7 @@ const server = new GraphQLServer({
 });
 
 server.express.use(logger('dev'));
+server.express.use(cors({ origin: 'http://localhost:3000' }));
+server.express.post('/api/upload', uploadMiddleware, uploadController);
 
 server.start({ port: PORT }, () => console.log(`sever http://localhost:${PORT}`));
